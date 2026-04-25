e9804bbc0bda8e2f4bebce3402c4b91aefe09b15
noukls <ta3daid@gmail.com>
2026-04-25 09:56:22 +0000

    l

---
+++ cell03/ex00/to25.py
@@ -0,0 +1,11 @@
+A = int(input())
+if A >= 25 :
+    print('Error')
+while A <25 :
+    A = A+1
+    if A == 25 : break
+    
+#    A = input()
+#    while A < 25 :
+#  A = A+1
+#        if A == 25 : break
