cd3a7f864ed9afdb8f5834da75fc2afc64009579
noukls <ta3daid@gmail.com>
2026-04-26 08:56:40 +0000

    ;

--- cell03/ex03/advanced_mult.py
+++ cell03/ex03/advanced_mult.py
@@ -1,6 +1,8 @@
 i = 0
 j = 0
 while i <= 10 :
-    i = i+1
-while j <= 10 :
-    print('Table de' + str(i),i*j)
+    print('Table de '+ str(i))
+    while j <=10 :
+        print(i*j)
+        j = j+1
+        if j == 10 : i = i+1
