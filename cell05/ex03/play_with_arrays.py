93b48622f54d843031b602b8f71c73169744ca8f
noukls <ta3daid@gmail.com>
2026-04-27 06:34:00 +0000

    l

--- cell05/ex03/play_with_arrays.py
+++ cell05/ex03/play_with_arrays.py
@@ -1,8 +1,8 @@
 import numpy as np
 x = int(input())
 A = np.zeros(x)
-B = np.full((1,x),2)
+B = np.full((x),2)
 for i in range(x) :
     A[i] = input()
 print(A)
-print(A+B)
+print(set(A + B))
---
+++ cell05/ex04/parameters.py
@@ -0,0 +1,5 @@
+import sys
+#for i in sys.argv :
+  #  print('Argument {} = ',sys.argv[i])
+#    i = i+1
+print('number of parameters = ',len(sys.argv))
