0de5f2c4e984e5f172f6e917935f593713a32bf4
noukls <ta3daid@gmail.com>
2026-04-28 01:01:05 +0000

    l

--- miniproject/main2.py
+++ miniproject/main2.py
@@ -1,15 +1,13 @@
-PIECES = {
-    'wK': '♔', 'wQ': '♕', 'wR': '♖', 'wB': '♗', 'wN': '♘', 'wP': '♙',
-    'bK': '♚', 'bQ': '♛', 'bR': '♜', 'bB': '♝', 'bN': '♞', 'bP': '♟',
-}
-def init_board():
-    return [
-        ['bR','bN','bB','bQ','bK','bB','bN','bR'],
-        ['bP','bP','bP','bP','bP','bP','bP','bP'],
-        [None]*8,
-        [None]*8,
-        [None]*8,
-        [None]*8,
-        ['wP','wP','wP','wP','wP','wP','wP','wP'],
-        ['wR','wN','wB','wQ','wK','wB','wN','wR'],
-    ]
+from checkmate import checkmate
+
+def main():
+    board = """\
+R...
+.K..
+..P.
+....\
+"""
+    checkmate(board)
+
+if __name__ == "__main__":
+    main()
