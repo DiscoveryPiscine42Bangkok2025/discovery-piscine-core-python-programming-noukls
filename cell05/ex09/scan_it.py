170909fc39565f620eea651924ff803c8fb7c3b6
noukls <ta3daid@gmail.com>
2026-04-28 14:02:21 +0000

    l

--- cell05/ex09/scan_it.py
+++ cell05/ex09/scan_it.py
@@ -1,2 +1,13 @@
 import re
-mat
+import sys
+key_word = sys.argv[1]
+text = sys.argv[2]
+matches = re.findall(key_word,text)
+print(matches)
+if len(sys.argv) != 3 :
+    print('none')
+elif matches == [] :
+    print('none')
+else :
+    print(len(matches)) 
+    
