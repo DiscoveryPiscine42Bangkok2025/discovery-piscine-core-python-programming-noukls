1167ba6abf47def05484143e4bc404608dff0b24
noukls <ta3daid@gmail.com>
2026-04-28 14:45:34 +0000

    l

---
+++ cell05/ex11/count_it.py
@@ -0,0 +1,8 @@
+import sys
+i = 1
+print('Parameters : ',len(sys.argv[1:]))
+    if len(sys.argv[1:] = 0) : 
+        print('none')
+    else : 
+        for i in sys.rgv[1:] :
+        print(len(sys.argv[i]))
