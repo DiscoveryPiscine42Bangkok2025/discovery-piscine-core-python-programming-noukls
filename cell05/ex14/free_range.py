6b7694c663021e182e8b5d9bf49db9722532bfe9
noukls <ta3daid@gmail.com>
2026-04-30 12:16:58 +0000

    l

--- cell05/ex14/free_range.py
+++ cell05/ex14/free_range.py
@@ -2,11 +2,12 @@ import sys
 import numpy as np
 i = int(sys.argv[1])
 x = int(sys.argv[2])
-A = np.array[x-i+1]
+A = np.array(x-i+1)
+a = 0
 if len(sys.argv) != 3 :
     print('none')
 else :
     for i in range(x) :
-        A[]
-        i = i+1
+        A[a] = i
+        a = a+1
 print(A)
