40c0fa4dcb7455111d6e176a688079697f22ebe4
noukls <ta3daid@gmail.com>
2026-04-30 11:31:02 +0000

    l

---
+++ cell05/ex14/free_range.py
@@ -0,0 +1,12 @@
+import sys
+import numpy as np
+i = int(sys.argv[1])
+x = int(sys.argv[2])
+A = np.array[x-i+1]
+if len(sys.argv) != 3 :
+    print('none')
+else :
+    for i in range(x) :
+        A[]
+        i = i+1
+print(A)
