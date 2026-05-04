780376b83b2d2dc6ae2400e0b0f0bfa7def0925e
noukls <ta3daid@gmail.com>
2026-05-04 11:08:32 +0000

    l

--- cell06/ex02/lowcase_it.py
+++ cell06/ex02/lowcase_it.py
@@ -1,3 +1,11 @@
+import sys
 def downcase_it() :
     
+        if len(sys.argv) < 2 :
+            print('none')
+        else : 
+            for i in range(len(sys.argv)) :
+                i = i+1
+                print(sys.argv[i].lower())
+downcase_it()
  
