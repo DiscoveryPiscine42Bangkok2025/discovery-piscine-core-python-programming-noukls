999f3007a68b6b93e37b0429f4e7dca45e84db3f
noukls <ta3daid@gmail.com>
2026-05-05 15:56:06 +0000

    l

---
+++ cell07/ex00/your_namebook.py
@@ -0,0 +1,17 @@
+
+# thisdict = {
+#   "brand": "Ford",
+#   "model": "Mustang",
+#   "year": 1964
+# }
+# x = str(thisdict.keys())
+# y = str(thisdict.values())
+# print(type(x))
+# print(x + y)
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
