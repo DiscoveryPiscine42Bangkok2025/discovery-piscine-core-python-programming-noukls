3f736b513313a4e270b8e0f573edcb9574068bc2
noukls <ta3daid@gmail.com>
2026-05-05 15:56:33 +0000

    l

---
+++ cell06/ex03/greetings_for_all.py
@@ -0,0 +1,9 @@
+def greetings(n) :
+    
+    if n == str :
+        print(f"Hello {n}")
+    elif n != str :
+        print('Error, It was not a name')
+    elif n == () :
+        print('Hello,noble stranger')
+greetings("Alex")
